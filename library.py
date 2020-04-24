885774c71baaa83bf4b3a841ed4d2a5d48877b08
Erik Tjong Kim Sang <erikt@xs4all.nl>
2020-04-24 17:16:55 +0200

    renamed date constants

--- library.py
+++ library.py
@@ -18,21 +18,22 @@ def getTweetText(jsonData):
 
 from datetime import datetime, timedelta
 
-DATEFORMAT = "%a %b %d %H:%M:%S %z %Y"
-SUMMERTIMEDATE = datetime.strptime("Sun Mar 29 02:00:00 +0000 2020",DATEFORMAT)
-WINTERTIMEDATE = datetime.strptime("Sun Oct 25 03:00:00 +0000 2020",DATEFORMAT)
+DATEFORMATTWITTER = "%a %b %d %H:%M:%S %z %Y"
+DATEFORMATGTD = "%Y%m%d"
+SUMMERTIMEDATE = datetime.strptime("Sun Mar 29 02:00:00 +0000 2020",DATEFORMATTWITTER)
+WINTERTIMEDATE = datetime.strptime("Sun Oct 25 03:00:00 +0000 2020",DATEFORMATTWITTER)
 
-def getTweetDate(jsonData):
+def getTweetDate(jsonData,dateFormat=DATEFORMATGTD):
     dateString = jsonData["created_at"]
-    dateData = datetime.strptime(dateString,DATEFORMAT)+timedelta(hours=1)
+    dateData = datetime.strptime(dateString,DATEFORMATTWITTER)+timedelta(hours=1)
     if dateData >= SUMMERTIMEDATE:
         if dateData >= WINTERTIMEDATE: sys.exit("cannot happen")
         dateData += timedelta(hours=1)
-    return(dateData.strftime("%Y%m%d"))
+    return(dateData.strftime(dateFormat))
 
-DATEFORMAT = "%Y%m%d"
+DATEFORMATSATD = "%Y%m%d"
 
-def stringArrayToDates(stringList,dateFormat=DATEFORMAT):
+def stringArrayToDates(stringList,dateFormat=DATEFORMATSATD):
     return([datetime.strptime(str(date),dateFormat) for date in stringList])
 
 def main(argv): pass
